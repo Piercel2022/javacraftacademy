0772f643d00375ca8250ed13b18d45f27a6433f4
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-12 07:32:19 +0200

    Add the entry point for the lesson component

---
+++ frontend/src/pages/Lesson/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Lesson = () => {
+  return (
+    <div className="lesson-page">
+      <h1>Lesson</h1>
+      <p>Lesson page coming soon...</p>
+    </div>
+  );
+};
+
+export default Lesson;
