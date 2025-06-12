4de0c4dd5d4debee07d2e1c2ad18117da4d358aa
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-12 07:28:24 +0200

    Add the entry point for the course detail component

---
+++ frontend/src/pages/CourseDetail/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CourseDetail = () => {
+  return (
+    <div className="course-detail-page">
+      <h1>Course Detail</h1>
+      <p>Course detail page coming soon...</p>
+    </div>
+  );
+};
+
+export default CourseDetail;
