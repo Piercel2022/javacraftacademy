f8a3e4b61d281d55409c7b2fd80cb021dd022e11
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-12 07:36:45 +0200

    Add the entry point for the profile component

---
+++ frontend/src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="profile-page">
+      <h1>Profile</h1>
+      <p>Profile page coming soon...</p>
+    </div>
+  );
+};
+
+export default Profile;
