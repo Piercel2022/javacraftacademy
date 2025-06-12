75019248e63dc5fd1fa790b6f35efefbb838a49d
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-12 07:37:51 +0200

    add the entry point for the progress component

---
+++ frontend/src/pages/Progress/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Progress = () => {
+  return (
+    <div className="progress-page">
+      <h1>Progress</h1>
+      <p>Progress page coming soon...</p>
+    </div>
+  );
+};
+
+export default Progress;
