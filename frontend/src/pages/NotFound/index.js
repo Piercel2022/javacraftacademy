98fd3732a3c7f01b5db33345d9bc1f8abd9f5fc3
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-15 15:55:06 +0200

    Update: NotFound index.js updated

--- frontend/src/pages/NotFound/index.js
+++ frontend/src/pages/NotFound/index.js
@@ -1,12 +1,1 @@
-import React from 'react';
-
-const NotFound = () => {
-  return (
-    <div className="not-found-page">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-    </div>
-  );
-};
-
-export default NotFound;
+export { default } from './NotFound'
