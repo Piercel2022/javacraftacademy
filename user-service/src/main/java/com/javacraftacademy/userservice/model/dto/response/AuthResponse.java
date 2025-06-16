10e035176d5bfd5022025d2eabe0929364386782
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-16 13:12:39 +0200

    refactor(response): update AuthResponse fields or methods

--- user-service/src/main/java/com/javacraftacademy/userservice/model/dto/response/AuthResponse.java
+++ user-service/src/main/java/com/javacraftacademy/userservice/model/dto/response/AuthResponse.java
@@ -1,2 +1,56 @@
 package com.javacraftacademy.userservice.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    
+    @JsonProperty("access_token")
+    private String accessToken;
+    
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    
+    @JsonProperty("token_type")
+    private String tokenType;
+    
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+    
+    @JsonProperty("user_info")
+    private UserInfo userInfo;
+    
+    @JsonProperty("timestamp")
+    private LocalDateTime timestamp;
+    
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserInfo {
+        private Long id;
+        private String email;
+        private String firstName;
+        private String lastName;
+        private String role;
+        private boolean emailVerified;
+    }
+    
+    public AuthResponse(String accessToken, String refreshToken, String tokenType, Long expiresIn, UserInfo userInfo) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.tokenType = tokenType;
+        this.expiresIn = expiresIn;
+        this.userInfo = userInfo;
+        this.timestamp = LocalDateTime.now();
+    }
+}
