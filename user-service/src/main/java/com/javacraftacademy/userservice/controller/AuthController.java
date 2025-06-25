f70226c4b9bf47d24d2e4ce4917eb5b6597f7250
Piercel2022 <piercel.coder2022@gmail.com>
2025-06-25 12:18:55 +0200

    🔧 modif: AuthController.java

--- user-service/src/main/java/com/javacraftacademy/userservice/controller/AuthController.java
+++ user-service/src/main/java/com/javacraftacademy/userservice/controller/AuthController.java
@@ -10,8 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/auth")
